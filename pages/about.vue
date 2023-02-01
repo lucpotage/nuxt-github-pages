dd4e4d0b7bbea7d594c873fda3e5b31c7b7e8fcf
Luc Potage <luc.potage@gmail.com>
2023-02-01 22:16:48 +0100

    content: add two pages and images

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1>About</h1>
+    <NuxtLink to="/">Home</NuxtLink>
+    <img src="/v2osk-1Z2niiBPg5A-unsplash.jpg" loading="lazy">
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <NuxtLink to="/about">About</NuxtLink>
+    <img src="/qingbao-meng-01_igFr7hd4-unsplash.jpg" loading="lazy">
+  </main>
+</template>
