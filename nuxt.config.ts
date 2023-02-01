be6c2fda179c4f92849523bc29de644ed5baa835
Luc Potage <luc.potage@gmail.com>
2023-02-01 22:33:05 +0100

    config: replace router.base by app.baseURL option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  router: {
-    base: '/nuxt-github-pages/'
+  app: {
+    baseURL: '/nuxt-github-pages/'
   }
 })
